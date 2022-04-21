9777eb9f8047a37beba1ac01f8c3c4ef3add8f61
김나영 <ny3021@gmail.con>
2022-04-22 01:51:05 +0900

    0421

---
+++ 2022.04/0421/1076_저항.py
@@ -0,0 +1,10 @@
+color = ['black', 'brown', 'red', 
+'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+f = color.index(input())
+s = color.index(input())
+t = color.index(input())
+
+r = int(str(f) + str(s)) * (10 ** t)
+
+print(r)
