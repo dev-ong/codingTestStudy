3c155904bf0dbffde4f8af574511539e17c92007
김나영 <ny3021@gmail.con>
2022-05-04 01:49:52 +0900

    0503

---
+++ 2022.05/0503/1037_약수.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+a = list(map(int, input().split()))
+
+a_max = max(a)
+
+a_min = min(a)
+
+print(a_max * a_min)
---
+++ 2022.05/0503/10867_중복 빼고 정렬하기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+arr = list(set(arr))
+
+arr.sort()
+
+for i in arr:
+    print(i, end=' ')
